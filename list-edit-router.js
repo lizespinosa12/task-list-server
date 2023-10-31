907f833a4ad53f766c15a71748c892257fab46a2
liz <lizethe1292@gmail.com>
2023-10-31 16:05:40 -0500

    he agregado middlewares a los archivos list-view-router y list-edit-router

--- list-edit-router.js
+++ list-edit-router.js
@@ -1,18 +1,42 @@
 const express = require('express');
 const listEditRouter = express.Router();
-const uuid = require('uuid'); 
+const uuid = require('uuid');
+
+
+function validateTask(task) {
+  return task && task.description && typeof task.description === 'string';
+}
+
+listEditRouter.use((req, res, next) => {
+  
+  if (req.method !== 'POST' && req.method !== 'PUT') {
+    return res.status(405).json({ message: 'Método HTTP no permitido' });
+  }
+  next();
+});
+
+listEditRouter.use((req, res, next) => {
+  
+  if (req.method === 'POST' && (!req.body || !validateTask(req.body))) {
+    return res.status(400).json({ message: 'Solicitud POST inválida' });
+  }
+  
+  if (req.method === 'PUT' && (!req.body || !validateTask(req.body))) {
+    return res.status(400).json({ message: 'Solicitud PUT inválida' });
+  }
+  next();
+});
 
 listEditRouter.post('/create', (req, res) => {
   const { description } = req.body;
   const newTask = {
-    id: uuid.v4(), 
-    isCompleted: false, 
+    id: uuid.v4(),
+    isCompleted: false,
     description,
   };
   tasks.push(newTask);
   res.json(newTask);
 });
-
 
 listEditRouter.delete('/:taskId', (req, res) => {
   const taskId = req.params.taskId;
@@ -25,7 +49,6 @@ listEditRouter.delete('/:taskId', (req, res) => {
   }
 });
 
-// Actualizar una tarea
 listEditRouter.put('/:taskId', (req, res) => {
   const taskId = req.params.taskId;
   const { description, isCompleted } = req.body;
--- list-view-router.js
+++ list-view-router.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const listViewRouter = express.Router();
 
+
+function validateParameters(req, res, next) {
+  const { param } = req.query;
+  
+  if (!param || (param !== 'complete' && param !== 'incomplete')) {
+    return res.status(400).json({ error: 'Parámetro no válido' });
+  }
+  
+  
+  next();
+}
+
+listViewRouter.use(validateParameters);
+
 listViewRouter.get('/complete', (req, res) => {
   try {
     const completedTasks = tasks.filter((task) => task.isCompleted);
     res.json(completedTasks);
   } catch (error) {
-    
     console.error(error);
     res.status(500).json({ error: 'Error interno del servidor' });
   }
@@ -17,7 +30,6 @@ listViewRouter.get('/incomplete', (req, res) => {
     const incompleteTasks = tasks.filter((task) => !task.isCompleted);
     res.json(incompleteTasks);
   } catch (error) {
-    
     console.error(error);
     res.status(500).json({ error: 'Error interno del servidor' });
   }
