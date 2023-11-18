470c997b706fa12ad3a9ae72e8c7f77ca735aeb9
Liz <lizethe1292@gmail.com>
2023-11-18 12:09:30 -0500

    he modificado App.js y agregué el archivo .env

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+const uuid = require('uuid');
+dotenv.config();
+
 const app = express();
 const port = 4000;
 
@@ -6,7 +11,6 @@ app.use(express.json());
 
 const listViewRouter = require('./list-view-router');
 const listEditRouter = require('./list-edit-router');
-
 app.use('/list-view', listViewRouter);
 app.use('/list-edit', listEditRouter);
 
@@ -37,6 +41,46 @@ const tasks = [
     description: 'Entregar pedidos',
   },
 ];
+
+const users = [
+  { username: 'usuario1', password: 'contrasena1' },
+  { username: 'usuario2', password: 'contrasena2' },
+  // Agrega más usuarios según sea necesario
+];
+
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  const user = users.find((user) => user.username === username && user.password === password);
+
+  if (!user) {
+    return res.status(401).json({ error: 'Credenciales inválidas', success: false });
+  }
+
+  const token = jwt.sign({ username: user.username }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+  res.json({ token, success: true });
+});
+
+app.get('/protected', authenticateToken, (req, res) => {
+  res.json({ message: 'Ruta protegida alcanzada', success: true });
+});
+
+function authenticateToken(req, res, next) {
+  const token = req.headers['authorization'];
+
+  if (!token) {
+    return res.status(401).json({ error: 'Token no proporcionado', success: false });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+      return res.status(403).json({ error: 'Token inválido', success: false });
+    }
+    req.user = user;
+    next();
+  });
+}
 
 app.get('/tasks', (req, res) => {
   res.json(tasks);
